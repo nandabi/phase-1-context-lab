5a822ca6d3a9c66477ca8b9cc33b7e53a47e222c
nandabi <valerie.nandabi@student.moringaschool.com>
2024-01-16 18:44:46 +0300

    commit message

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createEmployeeRecord(array) {
+    return{
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array [3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords(array) {
+    return array.map(createEmployeeRecord)
+}
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split('')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour,10),
+        date
+    })
+    return this 
+}
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+function hoursWorkedOnDate(dateStamp) {
+    let timeIn = this.timeInEvents.find(e => e.date == dateStamp)
+    let timeOut = this.timeOutEvents.find(e => e.date == dateStamp)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+function wagesEarnedOnDate(dateStamp) {
+    return hoursWorkedOnDate.call(this, dateSytamp) * this.payPerHour
+}
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(e => e.firstName)
+}
+function calculatePayroll(array) {
+    return array.reduce((total, employee) => 
+    total + allWagesFor.call(employee),0)
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
